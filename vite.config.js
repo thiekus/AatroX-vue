1483ef8454a04a52852f8b703619b0831e141336
Faris Khowarizmi <13563072+thiekus@users.noreply.github.com>
2024-02-15 16:29:10 +0700

    Update packages and add legacy code shim

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
+import legacy from '@vitejs/plugin-legacy'
 import { resolve } from 'path'
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        eslintPlugin(),
+        legacy({
+            targets: ['defaults', 'Firefox >= 52'],
+            additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+        }),
+    ],
     resolve: {
         alias: {
             '@': resolve(__dirname, 'src'),
